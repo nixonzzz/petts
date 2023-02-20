67b987785728e27c845c005d18adf87e519a8b15
Nikita <80811715+nixonzzz@users.noreply.github.com>
2023-02-20 12:05:03 +0500

    1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,16 @@
-import React, {Suspense} from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import React from 'react';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
-
+    const { theme } = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
-            </Suspense>
+            <Navbar />
+            <AppRouter />
         </div>
     );
 };
